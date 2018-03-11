f36633b09fb8e3421a68231de3e604b2527e3e2a
Rajan Patil <rpatil@equalexperts.com>
2018-03-11 13:40:17 +0530

    Added current time to hello world message.

--- src/com/helloworld/HelloWorld.java
+++ src/com/helloworld/HelloWorld.java
@@ -1,8 +1,11 @@
 package com.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
+
     public static void main(String[] args){
-        System.out.println("HelloWorld...");
+        System.out.println("Hello World, current time is "+ new Date());
     }
 
 }
